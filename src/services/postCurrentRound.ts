f69d876030e8d26586ac5542553ca12bcd871107
Jeferson <jefvivasaragao@gmail.com>
2024-07-29 16:01:12 -0300

    fix putItemCommand

--- src/services/postCurrentRound.ts
+++ src/services/postCurrentRound.ts
@@ -1,18 +1,21 @@
-import { DynamoDBClient, UpdateItemCommand } from "@aws-sdk/client-dynamodb";
+import { DynamoDBClient, PutItemCommand } from "@aws-sdk/client-dynamodb";
 import { marshall } from "@aws-sdk/util-dynamodb";
 
 const ddbClient = new DynamoDBClient({ region: "sa-east-1" });
 
 export async function postCurrentRound(): Promise<void> {
-  const PutItemParams = {
-    TableName: "Cartola",
-    Key: marshall({ id: "RoundNumber" }),
-    UpdateExpression: "SET currentRound = :currentRound",
-    ExpressionAttributeValues: marshall({ ":currentRound": 1 }),
-  };
+  const item = marshall({
+    id: "RoundNumber",
+    currentRound: 1,
+  });
 
   try {
-    await ddbClient.send(new UpdateItemCommand(PutItemParams));
+    await ddbClient.send(
+      new PutItemCommand({
+        TableName: "Cartola",
+        Item: item,
+      })
+    );
     return;
   } catch (e) {
     console.error(e);
